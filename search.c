ff3f2ba8008c6a3796eb9285eb14ddaf9c2c0a53
Pieter Wuille <pieter.wuille@gmail.com>
2017-05-30 15:49:39 -0700

    better search

--- search.c
+++ search.c
@@ -5,68 +5,59 @@
 #include <assert.h>
 #include <gmp.h>
 
-#define BITS 256
-#define SPACING 16
-#define MAXTERMS 30
+#define BITS (4096)
+#define SPACING 1
+#define MAXTERMS (BITS/2-1)
 
 void pow2(mpz_t out, int n) {
     mpz_set_ui(out, 1);
     mpz_mul_2exp(out, out, n);
 }
 
-int main(int argc, char** argv) {
+uint64_t count = 0;
+int total = 1;
+int id = 0;
 
-    int total = argc > 1 ? strtoul(argv[1], NULL, 0) : 1;
-    int num = argc > 2 ? strtoul(argv[2], NULL, 0) : 0;
-    printf("CPU %i/%i\n", num, total);
-
-    mpz_t n, m, p;
-    mpz_init(n);
-    mpz_init(m);
-    mpz_init(p);
-    for (int bits = 0; bits <= MAXTERMS; ++bits) {
-        int cnt = 0;
-        for (int val = 1; val < (1ULL << MAXTERMS); val += 2) {
-            if (__builtin_popcount(val) != bits) continue;
-            for (int sign = 0; sign <= val ; ++sign) {
-                if (sign & ~val) continue;
-                if (__builtin_clz(sign) != __builtin_clz(val)) continue;
-                uint64_t hash = ((uint64_t)val) * 0x123456789ABCDEFULL ^ sign;
-                hash = (hash >> 32) | (hash << 32);
-                hash *= 0x123456789ABCDEFULL;
-                hash = (hash >> 32) | (hash << 32);
-                hash *= 0x123456789ABCDEFULL;
-                if ((hash % total) != num) continue;
-                ++cnt;
-                pow2(n, BITS);
-                for (int bit = 0; bit < MAXTERMS; ++bit) {
-                    if ((val >> bit) & 1) {
-                        pow2(m, bit * SPACING);
-                        if ((sign >> bit) & 1) {
-                            mpz_sub(n, n, m);
-                        } else {
-                            mpz_add(n, n, m);
-                        }
-                    }
-                }
-                mpz_sub_ui(p, n, 1);
-                mpz_fdiv_q_2exp(p, p, 1);
-                if (mpz_probab_prime_p(p, 1) && mpz_probab_prime_p(n, 15) && mpz_probab_prime_p(p, 15)) {
-                    printf("2^%i", BITS);
-                    for (int bit = MAXTERMS; bit >= 0; --bit) {
-                        if ((val >> bit) & 1) {
-                            if (bit) {
-                                printf(" %c 2^%i", (sign >> bit) & 1 ? '-' : '+', bit * SPACING);
-                            } else {
-                                printf(" %c 1", (sign >> bit) & 1 ? '-' : '+');
-                            }
-                        }
-                    }
-                    printf("\n");
-                }
+void recurse(mpz_t num, int min, int max, int bits) {
+    if (bits == 0) {
+        ++count;
+        if ((count % total) != id) return;
+        if ((count % 65536) == 0) printf(".");
+        if (mpz_probab_prime_p(num, 1)) {
+            mpz_t order;
+            mpz_init(order);
+            mpz_sub_ui(order, num, 1);
+            mpz_fdiv_q_2exp(order, order, 1);
+            if (mpz_probab_prime_p(order, 15) && mpz_probab_prime_p(num, 15)) {
+                gmp_printf("\nFOUND: %Zx\n", num);
             }
+            mpz_clear(order);
         }
-        printf("* %i bits: %i attempts\n", bits, cnt);
+        return;
+    }
+    mpz_t pow;
+    mpz_init(pow);
+    for (int pos = min + bits - 1; pos < max; ++pos) {
+        pow2(pow, pos);
+        mpz_sub(num, num, pow);
+        recurse(num, min, pos, bits - 1);
+        mpz_add(num, num, pow);
+    }
+    mpz_clear(pow);
+}
+
+int main(int argc, char** argv) {
+    setbuf(stdout, NULL);
+    total = argc > 1 ? strtoul(argv[1], NULL, 0) : 1;
+    id = argc > 2 ? strtoul(argv[2], NULL, 0) : 0;
+    printf("CPU %i/%i\n", id, total);
+
+    mpz_t n;
+    mpz_init(n);
+    pow2(n, BITS);
+    mpz_sub_ui(n, n, 1);
+    for (int bits = 0; bits < MAXTERMS; ++bits) {
+        recurse(n, 1, MAXTERMS, bits);
     }
 
     return 0;
